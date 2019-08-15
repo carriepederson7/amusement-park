cfdb20a10981054e51a4055779615f8376baf028
Carrie Schmid and Jacqueline Remmel <jacqueline@>
2019-08-15 10:15:41 -0700

    Add branching to show ride options based on height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-  
+  $("#enterHeight").submit(function(event) {
+    event.preventDefault();
+    var height = parseInt($("input#height").val());
+    console.log(height);
+    if (height > 34 && height < 54) {
+      $("#short").show();
+      $("h3").show();
+      $("#all").show();
+
+    } else if (height > 54 && height < 90) {
+      $("#extreme").show();
+      $("h3").show();
+      $("#all").show();
+      
+    } else {
+      alert("That height is not valid; try again.");
+    }
 
 
+  });
 });
